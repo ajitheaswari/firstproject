921639a560aeac9b2aeb6b0964396263897c8c64
ajitheaswari <ajitheaswari3@gmail.com>
2022-04-29 17:09:07 +0530

    final commit

--- first.java
+++ first.java
@@ -1,7 +1,7 @@
 public class MyClass { public static void main(String args[]) {
       int x=10;
       int y=25;
-      int z=x+y;
+      int d =30;
 
       System.out.println("Sum of x+y = " + z);
 <<<<<<< HEAD
